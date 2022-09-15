870920d8657ea8053b5c56c37b9f6c9b305c91bf
Daphne van Dijken <daphne.van_dijken@maxiv.lu.se>
2022-09-15 16:56:46 +0200

    cleanup and packaging

--- setup.py
+++ setup.py
@@ -1,42 +1,15 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# This file is part of the PandaPosTrig project
-#
-#
-#
-# Distributed under the terms of the GPL license.
-# See LICENSE.txt for more info.
 
-import os
-import sys
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup_dir = os.path.dirname(os.path.abspath(__file__))
 
-# make sure we use latest info from local code
-sys.path.insert(0, setup_dir)
-
-readme_filename = os.path.join(setup_dir, 'README.rst')
-with open(readme_filename) as file:
-    long_description = file.read()
-
-release_filename = os.path.join(setup_dir, 'PandaPosTrig', 'release.py')
-exec(open(release_filename).read())
-
-pack = ['PandaPosTrig']
-
-setup(name=name,
-      version=version,
-      description='',
-      packages=pack,
-      include_package_data=True,
-      test_suite="test",
-      entry_points={'console_scripts':['PandaPosTrig = PandaPosTrig:main']},
-      author='igor.beinik',
-      author_email='igor.beinik at maxiv.lu.se',
-      license='GPL',
-      long_description=long_description,
-      url='www.tango-controls.org',
-      platforms="Unix Like"
-      )
+setup(
+    name="tangods-pandapostrig",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    description="Panda position based triggering for STXM FPGA.",
+    url="https://gitlab.maxiv.lu.se/softimax/tangods-softimax-pandapostrig",
+    packages=find_packages(exclude=["tests", "*.tests.*", "tests.*", "tests"]),
+    include_package_data=True,
+    entry_points={"console_scripts": ["PandaPosTrig = PandaPosTrig:main",]},
+)
